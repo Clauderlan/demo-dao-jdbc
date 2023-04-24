0fdefe362564a7746ad2d7b580ec32e91d202e52
Clauderlan <clauderlanbatista@hotmail.com>
2023-04-24 17:36:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,18 @@
 package application;
 
-
-
-import db.DB;
 import entities.Department;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import entities.Seller;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department department = new Department(1, "TestMan");
+        Seller seller = new Seller(1, "TestMan", "Testman@gmail.com", new Date(), 3000.0, department);
         System.out.println(department);
+        System.out.println();
+        System.out.println(seller);
 
     }
 
